921f34b1702c7cdd3d37c11fd81a7214a4693ec1
Tanngar <31502612+Tanngar@users.noreply.github.com>
2019-05-29 15:52:35 +0200

    Add images

--- app/src/main/java/com/example/durak/Card.java
+++ app/src/main/java/com/example/durak/Card.java
@@ -1,10 +1,7 @@
 package com.example.durak;
 
 import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.graphics.Matrix;
-
-import java.util.ArrayList;
 
 public class Card {
     private int x;
@@ -13,38 +10,50 @@ public class Card {
     private String suit;
     private int rank;
     private int value;
-    private boolean faceup;
-    private boolean selected;
     private Bitmap cardFront;
     private Bitmap cardBack;
 
     public Card(int rank, String suit) {
         this.suit = suit;
         this.rank = rank;
-        this.faceup = false;
         this.value = rank;
     }
 
+    public void flip() {
+        if (currentBitmap == cardFront) {
+            currentBitmap = cardBack;
+        } else {
+            currentBitmap = cardFront;
+        }
+    }
+
+    public void rotateBitmap(float angle) {
+        Matrix matrix = new Matrix();
+        matrix.setRotate(angle);
+        currentBitmap = Bitmap.createBitmap(currentBitmap, 0, 0, currentBitmap.getWidth(), currentBitmap.getHeight(), matrix, true);
+        cardBack = Bitmap.createBitmap(cardBack, 0, 0, cardBack.getWidth(), cardBack.getHeight(), matrix, true);
+        cardFront = Bitmap.createBitmap(cardFront, 0, 0, cardFront.getWidth(), cardFront.getHeight(), matrix, true);
+    }
+
     public int getRank() { return rank; }
+
     public String getSuit() { return suit; }
+
     public int getX() { return x; }
+
     public void setX(int x) { this.x = x; }
 
     public int getY() { return y; }
+
     public void setY(int y) { this.y = y; }
 
-
     public Bitmap getCurrentBitmap() { return currentBitmap; }
+
     public void setCurrentBitmap(Bitmap currentBitmap) { this.currentBitmap = currentBitmap; }
 
-    public Bitmap getCardFront() { return cardFront; }
     public void setCardFront(Bitmap cardFront) { this.cardFront = cardFront; }
 
-    public Bitmap getCardBack() { return cardBack; }
     public void setCardBack(Bitmap cardBack) { this.cardBack = cardBack; }
-
-    public boolean getFaceup() { return faceup; }
-    public void setFaceup(boolean faceup) { this.faceup = faceup; }
 
     public int getValue() {
         return value;
@@ -54,20 +63,4 @@ public class Card {
         this.value = value;
     }
 
-    public void flip() {
-        if(currentBitmap == cardFront) {
-            currentBitmap = cardBack;
-        } else {
-            currentBitmap = cardFront;
-        }
-    }
-
-    public void rotateBitmap(float angle)
-    {
-        Matrix matrix = new Matrix();
-        matrix.setRotate(angle);
-        currentBitmap = Bitmap.createBitmap(currentBitmap, 0, 0, currentBitmap.getWidth(), currentBitmap.getHeight(), matrix, true);
-        cardBack = Bitmap.createBitmap(cardBack, 0, 0, cardBack.getWidth(), cardBack.getHeight(), matrix, true);
-        cardFront = Bitmap.createBitmap(cardFront, 0, 0, cardFront.getWidth(), cardFront.getHeight(), matrix, true);
-    }
 }
--- app/src/main/java/com/example/durak/GameActivity.java
+++ app/src/main/java/com/example/durak/GameActivity.java
@@ -1,23 +1,18 @@
 package com.example.durak;
 
-import android.app.Activity;
 import android.app.Dialog;
-import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-import java.util.ArrayList;
 
 public class GameActivity extends AppCompatActivity {
     private Game game;
     private GameView gameView;
-    private Card selected;
     private Dialog menuPopup;
     private TextView gameOverMessage;
 
@@ -58,6 +53,7 @@ public class GameActivity extends AppCompatActivity {
         menuButton.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v){
+                menuPopup.setCanceledOnTouchOutside(true);
                 menuPopup.show();
             }
         });
@@ -92,59 +88,12 @@ public class GameActivity extends AppCompatActivity {
             @Override
             public boolean onTouch(View v, MotionEvent event) {
                 if (event.getAction() == MotionEvent.ACTION_DOWN) {
-                    Log.d("AI", "Current player: "+game.getCurrentPlayer().isHuman()+" Attacker: "+game.getAttacker().isHuman()+" Defender: "+game.getDefender().isHuman());
-
-                    if(selected != null) {
-                        Log.d("deck", "Selected: " + selected.getSuit()+selected.getRank());
-                    } else {
-                        Log.d("deck", "Selected is null");
-                    }
-                    if (selected != null &&
-                            event.getX() > selected.getX() &&
-                            event.getX() < selected.getX() + selected.getCurrentBitmap().getWidth() &&
-                            event.getY() > selected.getY() &&
-                            event.getY() < selected.getY() + selected.getCurrentBitmap().getHeight()
-                    ) {
-                        if(game.moveIsValid(game.getCurrentPlayer(), selected)){
-                            game.transferCardToField(game.getCurrentPlayer(), selected);
-                            game.setCurrentCard(selected);
-                            game.swapCurrentPlayer();
-                            selected = null;
-                        } else {
-                            selected.setY(selected.getY() + selected.getCurrentBitmap().getHeight() / 2);
-                            selected = null;
-                        }
-                    }
-                    ArrayList<Card> hand = game.getPlayers().get(0).getHand();
-                    for (int i = hand.size() - 1; i >= 0; i--) {
-                        Card card = hand.get(i);
-
-                        if (event.getX() > card.getX() &&
-                                event.getX() < card.getX() + card.getCurrentBitmap().getWidth() &&
-                                event.getY() > card.getY() &&
-                                event.getY() < card.getY() + card.getCurrentBitmap().getHeight()
-                        ) {
-                            Log.d("deck", "Card clicked: " + card.getRank()+ card.getSuit());
-
-                            if (selected != null) {
-                                selected.setY(selected.getY() + selected.getCurrentBitmap().getHeight() / 2);
-                            }
-
-                            selected = card;
-                            card.setY(card.getY() - card.getCurrentBitmap().getHeight() / 2);
-                            break;
-                        } else {
-                            if (selected != null) {
-                                selected.setY(selected.getY() + selected.getCurrentBitmap().getHeight() / 2);
-                                selected = null;
-                            }
-                        }
-                    }
-                    gameView.invalidate();
+                    game.cardOnClick(event.getX(), event.getY());
                 }
                 return true;
             }
         });
+
         game = new Game(this, passButton, takeButton, gameOverMessage, menuPopup);
         game.setGameView(gameView);
         gameView.setGame(game);
